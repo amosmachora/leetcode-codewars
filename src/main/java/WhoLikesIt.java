38dd8e0d22fcd57635b0b2559487d0b3ef3ae628
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-07-07 13:11:07 +0300

    Who likes it Refactored with Switch

--- src/main/java/WhoLikesIt.java
+++ src/main/java/WhoLikesIt.java
@@ -5,24 +5,19 @@ public class WhoLikesIt {
     }
 
     public static String whoLikedIt(String... names) {
-        if (names.length == 0) {
-            return "no one likes this";
-        }
-        if (names.length == 1) {
-            return names[0] + " likes this";
-        }
-        if (names.length == 2) {
-            return names[0] + " and " + names[1] + " like this";
-        }
-        if (names.length == 3) {
-            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
-        }
-        if (names.length > 3) {
-            int extras = names.length - 2;
-            return names[0] + ", " + names[1] + " and " + extras + " others like this";
+        switch (names.length) {
+            case 0:
+                return "no one likes this";
+            case 1:
+                return names[0] + " likes this";
+            case 2:
+                return names[0] + " and " + names[1] + " like this";
+            case 3:
+                return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+            default:
+                int extras = names.length - 2;
+                return names[0] + ", " + names[1] + " and " + extras + " others like this";
         }
 
-
-        return "";
     }
 }
