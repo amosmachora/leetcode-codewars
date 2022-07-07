c812e95705c432befa8e9f27a5418b01b27ec387
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-07-07 13:03:05 +0300

    Who likes it

---
+++ src/main/java/WhoLikesIt.java
@@ -0,0 +1,28 @@
+public class WhoLikesIt {
+
+    public static void main(String[] args) {
+        System.out.println(whoLikedIt("Peter", "John", "Ken"));
+    }
+
+    public static String whoLikedIt(String... names) {
+        if (names.length == 0) {
+            return "no one likes this";
+        }
+        if (names.length == 1) {
+            return names[0] + " likes this";
+        }
+        if (names.length == 2) {
+            return names[0] + " and " + names[1] + " like this";
+        }
+        if (names.length == 3) {
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        }
+        if (names.length > 3) {
+            int extras = names.length - 2;
+            return names[0] + ", " + names[1] + " and " + extras + " others like this";
+        }
+
+
+        return "";
+    }
+}
